c163ad11eb61c81b371a2af9b2096c8395fb2431
ange0721 <angei.gerasimov@netcracker.com>
2021-08-31 17:44:29 +0300

    initial commit

--- main.go
+++ main.go
@@ -1,48 +1,23 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
-type Metric struct {
-	Data [][]*float64 `json:"data"`
-}
-
-var jsonStr = []byte(
-	`[{"data":[[ 1628586000000, null ], [ 1628586900000, 0.0 ], [ 1628587800000, null ]] },
-{"data":[[ 1728586000000, 0 ], [ 1728586900000, null ], [ 1728587800000, 0 ]] }]`)
+// Importing fmt and time
+import "fmt"
 
 func main() {
 
-	metricExternalResponse := make([]*Metric, 0)
-	var info [][]*float64
-	var data []map[string]interface{}
+	t := 1628729200000
 
-	if err := json.Unmarshal(jsonStr, &data); err != nil {
-		panic(err)
+	//timestamp const of needed round value (15 min)
+	dev := 900000
+
+	r := t % dev
+	var fiftenStamp int
+	if r != 0 {
+		fiftenStamp = t + (dev - r)
+	} else {
+		fiftenStamp = t
 	}
 
-	for index, _ := range data {
-		newData := data[index]["data"].([]interface{})
-		fmt.Println(newData)
-		for _, in := range newData {
-			var arr []*float64
-			//fmt.Println(in)
-
-			result, _ := json.Marshal(in)
-			fmt.Printf("marshalled result %v\n", string(result))
-			//rs := strings.ReplaceAll(string(result), "null", "-1")
-
-			json.Unmarshal([]byte(result), &arr)
-			info = append(info, arr)
-
-		}
-		metricExternalResponse = append(metricExternalResponse, &Metric{Data: info})
-		info = [][]*float64{}
-	}
-
-	result, _ := json.Marshal(metricExternalResponse)
-
-	fmt.Println(string(result))
+	// Prints output
+	fmt.Printf("The result after rounding 't' is: %v\n", fiftenStamp)
 }
